9d7b857f5b06996f2e60041adcf4dcbc07e26698
Benjamin <Williams>
2020-03-25 10:16:57 -0400

    Spring Boot Setup
    Updated `.gitignore` as well.

---
+++ src/test/java/org/wcci/evercraft/EvercraftApplicationTests.java
@@ -0,0 +1,13 @@
+package org.wcci.evercraft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvercraftApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
